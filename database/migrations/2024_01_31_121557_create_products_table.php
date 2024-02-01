1d7f1e25ddbefa420af50d4a0f468f6ab9b3383e
Hasanuzzaman Roky <roky484812@gmail.com>
2024-02-01 14:34:05 +0600

    create product migration

--- database/migrations/2024_01_31_121557_create_products_table.php
+++ database/migrations/2024_01_31_121557_create_products_table.php
@@ -13,8 +13,25 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
-
+            $table->string('title');
+            $table->integer('price');
+            $table->integer('distributor_price');
+            $table->string('product_code')->default(uniqid())->unique();
+            $table->string('thumbnail_image');
+            $table->bigInteger('view_count');
+            $table->integer('country_id');
+            $table->unsignedBigInteger('category_id');
+            $table->unsignedBigInteger('sub_category_id');
+            $table->unsignedBigInteger('author_id');
+            $table->string('short_description')->nullable();
+            $table->string('description')->nullable();
+            $table->boolean('is_variational')->default(false);
+            $table->boolean('status')->default(false);
             $table->timestamps();
+
+            $table->foreign('category_id')->references('id')->on('product_categories');
+            $table->foreign('sub_category_id')->references('id')->on('product_sub_categories');
+            $table->foreign('author_id')->references('id')->on('users');
         });
         
     }
