6c1b2e6632949b79a999186af997bf30eef8d31b
Hasanuzzaman Roky <roky484812@gmail.com>
2024-03-26 21:43:11 +0600

    meta data

--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -3,6 +3,11 @@
 <head>
 	<meta charset="UTF-8" />
 	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	<meta property="og:title" content="Dreamssel Ecommerce Login">
+    <meta property="og:description" content="For your fashion dream">
+    <meta property="og:image" content="{{ asset('assets/admin/images/brand/white_single_logo.svg') }}">
+    <meta property="og:url" content="https://dreamssel.com/">
+    <meta property="og:type" content="Ecommerce">
 	<script src="https://kit.fontawesome.com/64d58efce2.js" crossorigin="anonymous"></script>
 	<!--Favicon -->
 	<link rel="icon" href="{{ asset('assets/admin/images/brand/white_single_logo.svg') }}" type="image/x-icon" />
